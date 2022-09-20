f43340aa589d5b461764299e22880e11b3b910ee
qzhibin <1601256263@qq.com>
2022-09-21 00:51:11 +0800

    test01

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,13 +6,26 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import {
+  defineComponent,
+  reactive,
+  onActivated,
+  onBeforeMount,
+  onBeforeUnmount,
+  onBeforeUpdate,
+  onDeactivated
+} from "vue"
+import HelloWorld from "@/components/HelloWorld.vue" // @ is an alias to /src
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    HelloWorld,
-  },
-});
+    HelloWorld
+  }
+
+
+
+
+  
+})
 </script>
