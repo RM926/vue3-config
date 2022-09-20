1efe7dd65c93cc84c29dcea088b8489d2a1267a8
qzhibin <1601256263@qq.com>
2022-09-21 01:06:52 +0800

    fix(login): test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,16 +14,14 @@ import {
   onBeforeUnmount,
   onBeforeUpdate,
   onDeactivated
-} from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+} from "vue"
+import HelloWorld from "@/components/HelloWorld.vue" // @ is an alias to /src
 
 export default defineComponent({
   name: "HomeView",
 
-
-  
   components: {
     HelloWorld
-  },
+  }
 })
 </script>
